5a0f08796939da59b6b650fd16dae1d62081557e
Raymond <raytbrien@gmail.com>
2024-02-12 14:15:14 +0000

    Add intial get wordbank values function

--- run.py
+++ run.py
@@ -1,12 +1,25 @@
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
-
 import gspread
 from google.oauth2.service_account import Credentials
-import json
+from pprint import pprint
 
 SCOPE = [
-    'https://WWW.googleapis.com/auth/spreadsheets',
+    'https://www.googleapis.com/auth/spreadsheets',
     'https://www.googleapis.com/auth/drive.file',
     'https://www.googleapis.com/auth/drive'
 ]
 
+CREDS = Credentials.from_service_account_file('creds.json')
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+SHEET = GSPREAD_CLIENT.open('zombie_bingo')
+
+def get_sheet():
+    """
+    Returns a reference to the sheet
+    """
+    sales = SHEET.worksheet('wordbank')
+    data = sales.col_values(2)
+    pprint(data)
+
+get_sheet()
