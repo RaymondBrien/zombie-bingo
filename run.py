f60eba65847026b6688868efa38313e9f3eee99d
Raymond <raytbrien@gmail.com>
2024-02-12 12:23:47 +0000

    Add APIs for google sheets and drive

--- run.py
+++ run.py
@@ -1,1 +1,12 @@
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+import gspread
+from google.oauth2.service_account import Credentials
+import json
+
+SCOPE = [
+    'https://WWW.googleapis.com/auth/spreadsheets',
+    'https://www.googleapis.com/auth/drive.file',
+    'https://www.googleapis.com/auth/drive'
+]
+
